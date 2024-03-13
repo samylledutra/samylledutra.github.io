52a99b6d1278bc3bf8be31ff53e607a2186b67c9
samylledutra <133446460+samylledutra@users.noreply.github.com>
2024-03-13 19:49:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,54 +2,42 @@ window.addEventListener("load", function () {
 
     const idades = [20, 25, 30, 35];
 
-// a)
-document.write("Soma das idades: " + somaIdades + "<br>");
+    const somaIdades = idades.reduce((total, idade) => total + idade, 0);
+    const mediaIdades = somaIdades / idades.length;
+    const maiorIdade = idades.reduce((maior, idade) => idade > maior ? idade : maior);
+    const idadesImpares = idades.filter(idade => idade % 2 !== 0);
+    const todasMaioresDeIdade = idades.every(idade => idade >= 18);
 
-//b)
-document.write("Média aritmética das idades: " + mediaIdades.toFixed(2) + "<br>");
+    const valorInformado = parseInt(prompt("Informe um valor:", "19"));
+    const todasMaioresQueValor = idades.every(idade => idade >= valorInformado);
 
-//c)
-document.write("Maior idade: " + maiorIdade + "<br>");
+    const idadeDeterminada = parseInt(prompt("Informe uma idade:", "18"));
+    const idadesMaioresQueDeterminada = idades.filter(idade => idade >= idadeDeterminada);
 
-//d)
-document.write("Idades ímpares: " + idadesImpares.join(", ") + "<br>");
+    const somaIdadesMaioresQueDeterminada = idadesMaioresQueDeterminada.reduce((total, idade) => total + idade, 0);
+    const mediaIdadesMaioresQueDeterminada = somaIdadesMaioresQueDeterminada / idadesMaioresQueDeterminada.length;
 
-//e)
-document.write("Idades são maiores ou iguais a 18: " + todasMaioresDeIdade + "<br>");
+    // a)
+    document.write("Soma das idades: " + somaIdades + "<br>");
 
-//f)
-const valorInformado = parseInt(prompt("Informe um valor:"));
-document.write("Idades são maiores ou iguais a " + valorInformado + ": " + todasMaioresQueValor + "<br>");
+    //b)
+    document.write("Média aritmética das idades: " + mediaIdades.toFixed(2) + "<br>");
 
-//g)
-const idadeDeterminada = parseInt(prompt("Informe uma idade:"));
-document.write("Idades maiores ou iguais a " + idadeDeterminada + " são: " + idadesMaioresQueDeterminada.join(", ") + "<br>");
+    //c)
+    document.write("Maior idade: " + maiorIdade + "<br>");
 
-//h)
-document.write("Média das idades das pessoas com idades maiores ou iguais a " + idadeDeterminada + " é: " + mediaIdadesMaioresQueDeterminada.toFixed(2) + "<br>");
+    //d)
+    document.write("Idades ímpares: " + idadesImpares.join(", ") + "<br>");
 
+    //e)
+    document.write("Idades são maiores ou iguais a 18: " + todasMaioresDeIdade + "<br>");
 
-const somaIdades = idades.reduce((total, idade) => total + idade, 0);
+    //f)
+    document.write("Idades são maiores ou iguais a " + valorInformado + ": " + todasMaioresQueValor + "<br>");
 
-const mediaIdades = somaIdades / idades.length;
+    //g)
+    document.write("Idades maiores ou iguais a " + idadeDeterminada + " são: " + idadesMaioresQueDeterminada.join(", ") + "<br>");
 
-const maiorIdade = idades.reduce((maior, idade) => idade > maior ? idade : maior);
-
-const idadesImpares = idades.filter(idade => idade % 2 !== 0);
-
-const todasMaioresDeIdade = idades.every(idade => idade >= 18);
-
-
-const todasMaioresQueValor = idades.every(idade => idade >= valorInformado);
-
-
-const idadesMaioresQueDeterminada = idades.filter(idade => idade >= idadeDeterminada);
-
-
-const somaIdadesMaioresQueDeterminada = idadesMaioresQueDeterminada.reduce((total, idade) => total + idade, 0);
-const mediaIdadesMaioresQueDeterminada = somaIdadesMaioresQueDeterminada / idadesMaioresQueDeterminada.length;
-
-
-
-
+    //h)
+    document.write("Média das idades das pessoas com idades maiores ou iguais a " + idadeDeterminada + " é: " + mediaIdadesMaioresQueDeterminada.toFixed(2) + "<br>");
 });

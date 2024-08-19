0ea938c37b102225ff553de6f8a29e10fc8f28e0
samylledutra <samyalexsandra.dutra@gmail.com>
2024-08-18 21:27:01 -0300

    commit-secao-form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Secao_Form from './Secao_Form/Secao_Form.jsx';
+import  "./styles.css";
+
+
+function App() {
+  return (
+    <div>
+    <Secao_Form/>
+   </div>
+  );
+}
+
+export default App;
